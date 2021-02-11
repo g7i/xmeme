4247e698058b648513c9656fb1a0a47cd6745dc9
GouravSaini125 <mailtogouravsaini@gmail.com>
2021-02-11 10:56:19 +0530

    fine tuning

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update
 sudo apt-get install -y curl
 
-curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
-sudo apt-get install nodejs -y
+
+#curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh -y
+#
+#sudo apt-get install nodejs -y
