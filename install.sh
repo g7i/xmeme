bc0aefab58abff6db19499616334fe6af3f461cb
Gourav Saini <mailtogouravsaini@gmail.com>
2021-02-14 16:54:49 +0530

    slightly modified install script

--- install.sh
+++ install.sh
@@ -3,11 +3,6 @@
 sudo apt-get update
 sudo apt-get install -y curl
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-
-#curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh -y
-#
-#sudo apt-get install nodejs -y
+sudo apt-get install -y nodejs
